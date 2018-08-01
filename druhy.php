2f55dfa3e1cc811580897d33c6265f70d66633e4
janaeffenbergerova <30951437+janaeffenbergerova@users.noreply.github.com>
2018-08-01 17:06:38 +0200

    form repair

--- druhy.php
+++ druhy.php
@@ -1,3 +1,3 @@
-<? if ( Mail("moje@adresa", $predmet, $zprava, "From: " . $mail_odesilatele) )
+<? if ( Mail("effenbergerova94@centrum.cz", $predmet, $zprava, "From: " . $mail_odesilatele) )
 echo "Mail byl odeslán";
 else echo "Mail se nepodařilo odeslat"; ?>
